d3c8030410ea9e009124550e78cd0414eba69c09
Johnatan Agudelo Múnera <johnatan.fisica@gmail.com>
2021-12-07 21:07:31 -0500

    Añadir proyecto base en GraphQL con correcciones

---
+++ graphql/types.ts
@@ -0,0 +1,5 @@
+import { userTypes } from '../models/user/types';
+import { projectTypes } from '../models/project/types';
+import { enumTypes } from '../models/enums/types';
+
+export const types = [userTypes, projectTypes, enumTypes];
