56d7c6c55ee063ad77a418253f76cbd29f5e5e99
Andres2020Leonardo <andruuescobar@gmail.com>
2021-12-10 11:00:10 -0500

    backend principal

---
+++ graphql/types.ts
@@ -0,0 +1,5 @@
+import { userTypes } from '../models/user/types';
+import { projectTypes } from '../models/project/types';
+import { enumTypes } from '../models/enums/types';
+import { inscripcionTypes } from '../models/inscripcion/types';
+export const types = [userTypes, projectTypes, enumTypes, inscripcionTypes];
