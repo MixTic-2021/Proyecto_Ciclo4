4b4abbe92ebf1bbe20e8c755377eb4722cd6a687
Andres2020Leonardo <andruuescobar@gmail.com>
2021-12-11 10:09:51 -0500

    funsionando con avances

--- models/resolvers.ts
+++
@@ -1,4 +0,0 @@
-import { userResolvers } from '../models/user/resolvers';
-import { projectResolvers } from '../models/project/resolvers';
-import { resolverInscripciones } from '../models/inscripcion/resolvers';
-export const resolvers = [userResolvers, projectResolvers, resolverInscripciones];
