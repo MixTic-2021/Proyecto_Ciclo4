a32e263d62607e91d2716dd6bec9a1719642c3a2
Andres2020Leonardo <andruuescobar@gmail.com>
2021-12-13 21:50:31 -0500

    todos los cambios

--- models/user/types.ts
+++ models/user/types.ts
@@ -2,36 +2,30 @@ import { gql } from 'apollo-server-express';
 
 const userTypes = gql`
   scalar Date
-
   enum Enum_EstadoUsuario {
     PENDIENTE
     AUTORIZADO
     NO_AUTORIZADO
   }
-
   enum Enum_Rol {
     ESTUDIANTE
     LIDER
     ADMINISTRADOR
   }
-
   enum Enum_EstadoProyecto {
     ACTIVO
     INACTIVO
   }
-
   enum Enum_FaseProyecto {
     INICIADO
     DESARROLLO
     TERMINADO
     NULO
   }
-
   enum Enum_TipoObjetivo {
     GENERAL
     ESPECIFICO
   }
-
   type Usuario {
     _id: ID!
     nombre: String!
@@ -41,18 +35,15 @@ const userTypes = gql`
     rol: Enum_Rol!
     estado: Enum_EstadoUsuario
   }
-
   type Objetivo {
     _id: ID!
     descripcion: String!
     tipo: Enum_TipoObjetivo!
   }
-
   type Query {
     Usuarios: [Usuario]
     Usuario(_id: String!): Usuario
   }
-
   type Mutation {
     crearUsuario(
       nombre: String!
@@ -62,7 +53,6 @@ const userTypes = gql`
       rol: Enum_Rol!
       estado: Enum_EstadoUsuario
     ): Usuario
-
     editarUsuario(
       _id: String!
       nombre: String!
@@ -72,7 +62,6 @@ const userTypes = gql`
       rol: Enum_Rol!
       estado: Enum_EstadoUsuario
     ): Usuario
-
     eliminarUsuario(_id: String, correo: String): Usuario
   }
 `;
